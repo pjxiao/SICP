2444ddd83976c08fd65cd754fa4b34907ce36972
Takayuki Hirai <namidame.xiao.xing.mell.1.0.1x@gmail.com>
2015-10-30 00:00:35 +0900

    Nested mapping

--- x223.scm
+++ x223.scm
@@ -141,3 +141,26 @@
 
 (define (reverse sequence)
     (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+
+; nested mapping
+
+; generate pairs
+; (accumulate append
+;     nil
+;     (map (lambda (i) 
+;            (map (lambda (j) (list i j))
+;                 (enumerate-interval 1 (- i 1))))
+;          (enumerate-interval 1 n)))
+
+; Hint:
+; gosh> (append nil (list 1 2 3))
+; (1 2 3)
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
